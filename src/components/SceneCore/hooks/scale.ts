479783a7a59dd60b496011cd6277ac29ee85caff
Fire <aslslgood22@163.com>
2025-04-03 16:54:27 +0800

    feat: 根据鼠标缩放

--- src/components/SceneCore/hooks/scale.ts
+++ src/components/SceneCore/hooks/scale.ts
@@ -1,5 +1,5 @@
 import { ref } from 'vue'
-import { Container, Application } from 'pixi.js'
+import { Container, Application, Point } from 'pixi.js'
 import type { FederatedWheelEvent } from 'pixi.js'
 import Big from 'big.js'
 export function useScale({
@@ -17,19 +17,28 @@ export function useScale({
   const refScale = ref(targetNode.scale)
   const minScale = 0.5
   const maxScale = 5
+  // 根据鼠标位置,缩放
   const onWheel = (e: FederatedWheelEvent) => {
     const currentScale = new Big(refScale.value.x)
+
+    const beforeNodeScaleX = refScale.value.x
+    let afterNodeScaleX = beforeNodeScaleX
+    /* 需要计算变化之后位置 */
     let delta = 0.1
     if (e.deltaY < 0) {
       delta = -delta
     }
-    const newScale = currentScale.plus( delta)
+    const newScale = currentScale.plus(delta)
     if (newScale.gte(minScale) && newScale.lte(maxScale)) {
-      refScale.value.x = newScale.toNumber()
-      refScale.value.y = newScale.toNumber()
+      afterNodeScaleX = newScale.toNumber()
+      /*  */
+      calcPosition(e, beforeNodeScaleX, afterNodeScaleX, targetNode)
+      refScale.value.x = afterNodeScaleX
+      refScale.value.y = afterNodeScaleX
+      // targetNode.scale = refScale.value
     }
-    targetNode.scale = refScale.value
   }
+
   targetNode.interactive = true
   function addEvent() {
     app.stage.on('wheel', onWheel)
@@ -49,4 +58,33 @@ export function useScale({
     removeEvent,
     dispose,
   }
+}
+function calcPosition(
+  e: FederatedWheelEvent,
+  beforeNodeScaleX: number,
+  afterNodeScaleX: number,
+  targetNode: Container,
+) {
+  /* 实际原点坐标 */
+  const mousePosition2 = e.getLocalPosition(targetNode.parent, e.global.clone())
+
+  /* 关键行1 */
+  const uiTouchPos = {
+    x: mousePosition2.x - targetNode.position.x,
+    y: mousePosition2.y - targetNode.position.y,
+  }
+
+  const divPosX = uiTouchPos.x / beforeNodeScaleX
+  const divPoxY = uiTouchPos.y / beforeNodeScaleX
+   /* 2 */
+  const mulX = divPosX * afterNodeScaleX
+  const mulY = divPoxY * afterNodeScaleX
+
+  const newPos = {
+    x: mousePosition2.x - mulX,
+    y: mousePosition2.y - mulY,
+  }
+
+  targetNode.scale.set(afterNodeScaleX, afterNodeScaleX)
+  targetNode.position.set(newPos.x, newPos.y)
 }
