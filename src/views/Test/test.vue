8c718020a882c6e8f05acb1a81459753107c24b0
Fire <aslslgood22@163.com>
2025-04-15 17:21:27 +0800

    feat: 剔除 一点参数尝试

--- src/views/Test/test.vue
+++ src/views/Test/test.vue
@@ -9,9 +9,16 @@ import { Simple, SpatialHash, type DisplayObjectWithCulling } from 'pixi-cull'
 init()
 async function init() {
   const app = new PIXI.Application()
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ;(globalThis as any).__PIXI_APP__ = app
-  await app.init()
+
+  window.__PIXI_DEVTOOLS__ = {
+  app: app,
+  // If you are not using a pixi app, you can pass the renderer and stage directly
+  // renderer: myRenderer,
+  // stage: myStage,
+};
+  await app.init({
+    resizeTo: window,
+  })
   document.body.appendChild(app.canvas)
 
   // create viewport
@@ -28,8 +35,15 @@ async function init() {
   viewport.drag().pinch().wheel().decelerate().moveCenter(5000, 5000)
   const container = new PIXI.Container()
   viewport.addChild(container)
+
+  // viewport.screenWidth = window.innerWidth
+  // viewport.screenHeight = window.innerHeight
+  // viewport.resize()
+  // viewport.on('zoomed', (e) => {
+  //   console.log(e.viewport.scale)
+  // })
   // add red boxes
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 10000; i++) {
     const sprite = container.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
     sprite.tint = 0xff0000
     sprite.width = sprite.height = 100
